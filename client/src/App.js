dd93dac7b9c08e5cf0b0fba35680a0f8d73e199b
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-07-24 11:47:45 -0400

    axios create with auth token and private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* <nav>
+          <Link to="/">Login</Link>
+        </nav> */}
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubbles" component={BubblePage} />
       </div>
     </Router>
   );
